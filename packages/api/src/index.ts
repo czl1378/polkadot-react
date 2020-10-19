fe26c85dce8d06ff7cabd61bae7f8208997393b1
czl1378 <czl1378@126.com>
2020-10-19 22:51:25 +0800

    fix bugs

---
+++ packages/api/src/index.ts
@@ -0,0 +1,12 @@
+import Api, { DEFAULT_DECIMALS, api } from './Api';
+import ApiContext from './ApiContext';
+
+import registry from './typeRegistry';
+
+export {
+  api,
+  Api,
+  ApiContext,
+  DEFAULT_DECIMALS,
+  registry
+};
